0a85adbb263566271e7538714f88d2808589275b
unknown <Gusabary@126.com>
2019-07-03 21:15:15 +0800

    feat(myinfo): add top banner in myinfo screen

---
+++ front_end/src/components/myinfo/TopBanner.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button, Avatar } from 'react-native-elements'
+
+const styles = StyleSheet.create({
+    border: {
+        //borderWidth:1,
+    },
+    username: {
+        marginTop: 22,
+        marginLeft: 8,
+        padding: 10,
+        paddingBottom: 6,
+        fontSize: 24,
+        fontWeight: 'bold',
+        borderStyle: 'solid',
+        //borderWidth: 1,
+    },
+    feather: {
+        marginLeft: 12,
+        padding: 10,
+        paddingTop: 6,
+        //borderWidth:1
+    },
+    attendance: {
+        marginTop: 50,
+        marginRight: 34,
+        width: 70,
+        height:40
+    }
+})
+
+class TopBanner extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <View style={{ flexDirection: "row" }}>
+                    <Avatar
+                        rounded
+                        size="large"
+                        icon={{ name: 'user', color: 'orange', type: 'font-awesome' }}
+                        overlayContainerStyle={{ backgroundColor: 'cyan', flex: 4, borderWidth: 1 }}
+                        //onPress={() => console.log("Works!")}
+                        activeOpacity={0.7}
+                        containerStyle={{ marginTop: 25, marginLeft: 25, borderWidth: 1 }}
+                        showEditButton
+                    />
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text
+                            style={[styles.username, styles.border]}
+                        >
+                            Gusabary
+                        </Text>
+                        <Text
+                            style={[styles.feather, styles.border]}
+                        >
+                            羽毛：8
+                        </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row-reverse', flex: 1 }}>
+                        <Button containerStyle={[styles.attendance,styles.border]} title="签到" />
+                    </View>
+                </View>
+            </React.Fragment>
+        );
+    }
+}
+export default TopBanner;
--- front_end/src/pages/myinfo/MyInfoScreen.js
+++ front_end/src/pages/myinfo/MyInfoScreen.js
@@ -1,12 +1,45 @@
 import React from "react";
-import {Text, View} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Button, Avatar } from 'react-native-elements'
+import TopBanner from "../../components/myinfo/TopBanner";
+
+const styles = StyleSheet.create({
+    border: {
+        //borderWidth:1,
+    },
+    username: {
+        marginTop: 22,
+        marginLeft: 8,
+        padding: 10,
+        paddingBottom: 6,
+        fontSize: 24,
+        fontWeight: 'bold',
+        borderStyle: 'solid',
+        //borderWidth: 1,
+    },
+    feather: {
+        marginLeft: 12,
+        padding: 10,
+        paddingTop: 6,
+        //borderWidth:1
+    },
+    attendance: {
+        marginTop: 50,
+        marginRight: 34,
+        width: 70,
+        height:40
+    }
+})
 
 class SettingsScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>MyInfo!</Text>
-            </View>
+            <React.Fragment>
+                <TopBanner />
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>MyInfo!</Text>
+                </View>
+            </React.Fragment>
         );
     }
 }
