31932a32a21a1063fca63908e95b4b73309adcc6
zncai <zinuocai@gmail.com>
2019-07-03 16:40:25 +0800

    refactor: re-add basic project structure

--- front_end/index.js
+++ front_end/index.js
@@ -1,3 +1,4 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import App from './src/index'
 AppRegistry.registerComponent(appName, () => App);
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import App from './components/App'
+import Home from './pages/Home'
 import store from './store/index'
 
 export default class Root extends React.Component{
     render(){
         return(
             <Provider store={store}>
-                <App />
+                <Home />
             </Provider>
         )
     }
 }
-
