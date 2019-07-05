25fd4881ab1963db5da9c8a553f1b1d6b2a366ea
unknown <Gusabary@126.com>
2019-07-05 16:13:52 +0800

    feat: make detailed blocks touchable

--- front_end/src/pages/myinfo/index.js
+++ front_end/src/pages/myinfo/index.js
@@ -1,15 +1,31 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import InitialScreen from "./InitialScreen"
-import FollowingScreen from "./FollowingScreen"
-import FollowersScreen from "./FollowersScreen"
-import MutualFollowScreen from "./MutualFollowScreen";
+import FollowingScreen from "./Follow/FollowingScreen"
+import FollowersScreen from "./Follow/FollowersScreen"
+import MutualFollowScreen from "./Follow/MutualFollowScreen";
+import MessageScreen from "./Repository/MessageScreen";
+import HeanScreen from "./Repository/HeanScreen";
+import CollectionScreen from "./Repository/CollectionScreen";
+import DiaryScreen from "./Repository/DiaryScreen";
+import JournalScreen from "./Repository/JournalScreen";
+import SubmissionScreen from "./Repository/SubmissionScreen";
+import MoodReportScreen from "./Repository/MoodReportScreen";
+import CommentScreen from "./Repository/CommentScreen";
 
 const MyInfoScreen = createStackNavigator(
     {
         Initial: { screen: InitialScreen },
         Following: { screen: FollowingScreen },
         Followers: { screen: FollowersScreen },
-        MutualFollow: {screen: MutualFollowScreen}
+        MutualFollow: { screen: MutualFollowScreen },
+        Message: { screen: MessageScreen },
+        Hean: { screen: HeanScreen },
+        Collection: { screen: CollectionScreen },
+        Diary: { screen: DiaryScreen },
+        Journal: { screen: JournalScreen },
+        Submission: { screen: SubmissionScreen },
+        MoodReport: { screen: MoodReportScreen },
+        Comment: { screen: CommentScreen },
     },
     {
         initialRouteName: 'Initial'
