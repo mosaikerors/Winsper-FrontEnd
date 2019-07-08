c98b25746d57279c6c4a08d705391a6f5739854a
unknown <Gusabary@126.com>
2019-07-08 21:46:36 +0800

    feat: add superagent

---
+++ front_end/src/agent.js
@@ -0,0 +1,18 @@
+const requests = require('superagent');
+
+const API_ROOT = "http://47.103.0.246:7120"
+
+const user = {
+    sendCode: (phone) =>
+        requests.post(API_ROOT + "/user/user/sendCode")
+            .send({ phone })
+            .then(res => res.body)
+            .catch(err => err.response.body),
+    sigup: (token, phone, code, username, password) =>
+        requests.post(API_ROOT + "/user/user/signup")
+            .send({ token, phone, code, username, password })
+            .then(res => res.body)
+            .catch(err => err.response.body),
+}
+
+export default { user }
--- front_end/src/components/myinfo/Signup.js
+++ front_end/src/components/myinfo/Signup.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Text, View, StyleSheet, TextInput } from "react-native";
 import { Button, Avatar, Divider, Card } from 'react-native-elements'
+import agent from "../../agent"
+
+const requests = require('superagent');
 
 const styles = StyleSheet.create({
     border: {
@@ -18,6 +21,42 @@ const styles = StyleSheet.create({
 })
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: '',
+            phone: '',
+            code: '',
+            username: '',
+            password: '',
+        }
+        this.updateState = this.updateState.bind(this);
+        this.sendCode = this.sendCode.bind(this);
+        this.submit = this.submit.bind(this);
+    }
+
+    async sendCode() {
+        console.log(this.state.phone)
+        const response = await agent.user.sendCode(this.state.phone);
+        this.setState({
+            token: response.token
+        })
+        console.log(response)
+    }
+
+    async submit() {
+        const { token, phone, code, username, password } = this.state;
+        const response = await agent.user.sigup(token, phone, code, username, password);
+        console.log(response);
+    }
+
+    updateState(field, text) {
+        this.setState({
+            [field]: text
+        });
+    }
+
+
     render() {
         return (
             <React.Fragment>
@@ -25,24 +64,24 @@ class Signup extends React.Component {
                     <Card title="注册">
                         <View style={{ flexDirection: 'row' }}>
                             <Text>手机号</Text>
-                            <TextInput style={{ width: 300, borderWidth:1 }} />
+                            <TextInput style={{ width: 300, borderWidth: 1 }} onChangeText={text => this.updateState('phone', text)} />
                         </View>
                         <View style={{ flexDirection: 'row' }}>
                             <Text>验证码</Text>
-                            <TextInput style={{ width: 200, borderWidth:1 }} />
-                            <Button title="获取验证码" />
+                            <TextInput style={{ width: 200, borderWidth: 1 }} onChangeText={text => this.updateState('code', text)} />
+                            <Button title="获取验证码" onPress={this.sendCode} />
                         </View>
                         <View style={{ flexDirection: 'row' }}>
                             <Text>用户名</Text>
-                            <TextInput style={{ width: 300, borderWidth:1 }} />
+                            <TextInput style={{ width: 300, borderWidth: 1 }} onChangeText={text => this.updateState('username', text)} />
                         </View>
                         <View style={{ flexDirection: 'row' }}>
                             <Text>密码</Text>
-                            <TextInput style={{ width: 300, borderWidth:1 }} />
+                            <TextInput style={{ width: 300, borderWidth: 1 }} onChangeText={text => this.updateState('password', text)} />
                         </View>
                         <Button containerStyle={[styles.attendance, styles.checked]}
                             title="注册"
-                            onPress={() => this.props.navigation.dispatch(login)}
+                            onPress={ this.submit /*this.props.navigation.dispatch(login)*/ }
                         />
                     </Card>
                 </View>
