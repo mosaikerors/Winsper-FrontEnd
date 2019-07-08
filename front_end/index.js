ccaff591d76716403d889fa2b6920ae8792cab9f
unknown <Gusabary@126.com>
2019-07-08 16:32:41 +0800

    chore: add map package

--- front_end/index.js
+++ front_end/index.js
@@ -1,4 +1,4 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './App'
+import App from './src/index'
 AppRegistry.registerComponent(appName, () => App);
