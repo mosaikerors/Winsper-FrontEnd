29f05e4b0ce50256ca166bea1b6bbbb1a48122a3
zncai <zinuocai@gmail.com>
2019-07-23 18:53:07 +0800

    test: test sign up component

--- front_end/__tests__/components/myinfo/testSignup.js
+++ front_end/__tests__/components/myinfo/testSignup.js
@@ -5,7 +5,6 @@ import {configure, mount} from "enzyme/build";
 import Adapter from "enzyme-adapter-react-16/build";
 import {Provider} from "react-redux";
 import {store} from '../../../src/store/index'
-import {Signin} from "../../../src/components/myinfo/Signin";
 configure({adapter: new Adapter()});
 
 test("renders sign up correctly", ()=>{
@@ -17,12 +16,33 @@ test("renders sign up correctly", ()=>{
     expect(tree).toMatchSnapshot();
 });
 
-test("test function change phone state", ()=> {
+test("test function change input state", ()=> {
     const phone="13813141036";
     const code="000000";
     const text="cznczn";
     const wrapper = mount(
-        <Signin/>
+        <Signup/>
+    );
+    wrapper.find("Input").at(0).prop("onChangeText")(phone);
+    expect(wrapper.state().phone).toBe(phone);
+    wrapper.find("Button").at(0).prop("onPress")();
+    expect(wrapper.state().rescodeForSendCode).toBe(-1);
+    wrapper.find("Input").at(1).prop("onChangeText")(code);
+    expect(wrapper.state().code).toBe(code);
+    wrapper.find("Input").at(2).prop("onChangeText")(text);
+    expect(wrapper.state().username).toBe(text);
+    wrapper.find("Input").at(3).prop("onChangeText")(text);
+    expect(wrapper.state().password).toBe(text);
+    wrapper.find("Button").at(1).prop("onPress")();
+    expect(wrapper.state().rescodeForSignup).toBe(-1);
+});
+
+test("test function submit successfully", ()=> {
+    const phone="13813141036";
+    const code="000000";
+    const text="cznczn";
+    const wrapper = mount(
+        <Signup/>
     );
     wrapper.find("Input").at(0).prop("onChangeText")(phone);
     expect(wrapper.state().phone).toBe(phone);
@@ -32,7 +52,4 @@ test("test function change phone state", ()=> {
     expect(wrapper.state().username).toBe(text);
     wrapper.find("Input").at(3).prop("onChangeText")(text);
     expect(wrapper.state().password).toBe(text);
-
-
-
 });
