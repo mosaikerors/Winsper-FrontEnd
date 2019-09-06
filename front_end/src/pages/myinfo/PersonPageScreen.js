00639780006a6cdf9e73caab205c5e2a45c12764
unknown <Gusabary@126.com>
2019-09-06 22:57:25 +0800

    feat: delete follow bar of personal page

--- front_end/src/pages/myinfo/PersonPageScreen.js
+++ front_end/src/pages/myinfo/PersonPageScreen.js
@@ -57,8 +57,6 @@ class PersonPageScreen extends React.Component {
             <React.Fragment>
                 <TopBanner isMe={false} avatar={avatar} username={username} feather={feather} hasFollowed={hasFollowed} otherUId={otherUId} />
                 <Divider />
-                <FollowBanner navigation={this.props.navigation} mutualFollow={mutualFollow} following={following} followers={followers} />
-                <Divider />
                 {/* userInfo 里除了隐私设置还有其他一些信息比如 username，但是这里为了方便，选择将整个 userInfo 传进去，尽管只用到了其中的隐私设置 */}
                 <DetailedBlock navigation={this.props.navigation} privacy={this.state.userInfo} isMe={false} otherUId={otherUId} />
                 <Divider />
